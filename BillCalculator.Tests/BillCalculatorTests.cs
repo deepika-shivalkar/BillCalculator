8917fa6b2e28868301fd29313a45240375b1b771
Deepika Shivalkar <deepika.a.shivalkar@gmail.com>
2020-06-21 10:53:18 +0530

    Commit with test project

---
+++ BillCalculator.Tests/BillCalculatorTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BillCalculator.Tests
+{
+    public class BillCalculatorTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
